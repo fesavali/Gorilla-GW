9ec9bf7f025b4b2a4d8b695f62ee07f664e31fa2
fesavali <felixnzioki99@gmail.com>
2021-08-30 16:28:20 +0300

    Update image.php

--- wp-admin/includes/image.php
+++ wp-admin/includes/image.php
@@ -1,6 +1,6 @@
 <?php
 
-// work on images here, resizing, cropping, posting, 
+// work on images here, resizing, cropping, posting, pre
 /**
  * File contains all the administration image manipulation functions.
  *
