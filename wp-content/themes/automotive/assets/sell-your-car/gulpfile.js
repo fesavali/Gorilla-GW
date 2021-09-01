5f5cc798a92cff1c20b639c997c95c54adaf6f16
fesavali <felixnzioki99@gmail.com>
2021-09-01 16:38:50 +0300

    Update gulpfile.js

--- wp-content/themes/automotive/assets/sell-your-car/gulpfile.js
+++ wp-content/themes/automotive/assets/sell-your-car/gulpfile.js
@@ -1,4 +1,4 @@
-handle imagefile select, upload, validate, resize, showstatus, preview and save 
+handle imagefile select, upload, validate, resize, showstatus, preview and save to 
 // Init Gulp.
 var gulp = require('gulp')
   , uglify = require("gulp-uglify")
