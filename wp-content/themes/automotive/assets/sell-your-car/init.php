55cfe425b6d5f97e7ebbc46751400ac2f3e699a2
fesavali <felixnzioki99@gmail.com>
2021-09-02 16:16:03 +0300

    Update init.php

--- wp-content/themes/automotive/assets/sell-your-car/init.php
+++ wp-content/themes/automotive/assets/sell-your-car/init.php
@@ -1,5 +1,5 @@
 <?php
-user init upload form, sanitize fields, handle
+user init upload form, sanitize fields, handle file
 /**
  * @file
  * Initialization and Helper functions for the Sell Your Car form.
